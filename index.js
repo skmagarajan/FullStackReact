6628dff925e56ad0b05d07da1f861a7734a77242
skmagarajan <saravanakrishnan876@gmail.com>
2021-02-25 13:58:20 -0600

    Testing Google OAuth - Passport JS

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const express = require('express');
+const passport = require('passport');
+const keys = require('./config/keys');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
+
+passport.use(
+		new GoogleStrategy({
+				clientID: keys.googleClientID,
+				clientSecret: keys.googleClientSecret,
+				callbackURL: '/auth/google/callback'
+		}, 
+		accessToken => {
+				console.log(accessToken);	
+		})
+);
+
+app.get('/auth/google', 
+		passport.authenticate('google', {
+				scope: ['profie','email']
+		})
+);
 
 app.get('/',(req,res)=> {
    res.send({hi: 'SK'}); 
